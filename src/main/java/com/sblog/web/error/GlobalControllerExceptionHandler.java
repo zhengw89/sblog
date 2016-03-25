34e648c888db8f81973c147a85375da0c171c7c3
Zheng Wang <zhengw89@gmail.com>
2016-03-25 10:33:46 +0800

    添加异常处理

---
+++ src/main/java/com/sblog/web/error/GlobalControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.sblog.web.error;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+	@ExceptionHandler(NoHandlerFoundException.class)
+    public ModelAndView handleError404(Exception e)   {
+            ModelAndView mv = new ModelAndView("404");
+            mv.addObject("exception", e);  
+            return mv;
+    }
+	
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleError500(Exception e) {
+		ModelAndView mv = new ModelAndView("500");
+		mv.addObject("ex", e);
+		return mv;
+	}
+}
